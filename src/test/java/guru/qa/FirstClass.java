cbba71bfd3532cd00266564bfc619de67112146b
Ekaterina <ekat9615@gmail.com>
2022-05-30 20:12:01 +0300

    first commit

---
+++ src/test/java/guru/qa/FirstClass.java
@@ -0,0 +1,11 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstClass {
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
